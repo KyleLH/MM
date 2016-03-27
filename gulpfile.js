e6201d6fe63cea12359bd2b63f6cfd7a22466a6f
Kyle Holzinger <kylelholzinger@gmail.com>
2016-03-27 15:26:18 -0400

    [KH] oops

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var jshint = require("gulp-jshint");
 var scsslint = require("gulp-scss-lint");
 var gulp = require("gulp");
 var scss = require("gulp-scss");
+var autoprefixer = require("gulp-autoprefixer");
 var nodemon = require("gulp-nodemon");
 
 var paths = {
@@ -23,6 +24,10 @@ gulp.task("lint:scss", function () {
 gulp.task("scss", function () {
   return gulp.src(paths.scss)
     .pipe(scss())
+    .pipe(autoprefixer({
+      browsers: [ 'last 2 versions' ],
+      cascade: false
+    }))
     .pipe(gulp.dest("./public/assets/css"));
 });
 
